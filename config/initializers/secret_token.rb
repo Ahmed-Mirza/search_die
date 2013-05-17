371caa3ed2521247cc95d50ce6c91054a587fe51
Ahmed <ahmed_mirza_baig@hotmail.com>
2013-05-17 17:13:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchDie::Application.config.secret_token = '927f28d0cb4f2d53582d7c289c4b21116610d601ec257c102d1d4b0d92121ba152d6bd9184e0c665ebe219cae90151f40c7480ac2e44afc77c8cbc095bfaeb47'
