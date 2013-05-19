d94c41d8b6f6ae45ed4b8321b5b6c1fa1b513704
Ahmed <ahmed_mirza_baig@hotmail.com>
2013-05-19 02:10:41 -0400

    Trying to use search content with API

--- app/controllers/search_contents_controller.rb
+++ app/controllers/search_contents_controller.rb
@@ -37,7 +37,7 @@ class SearchContentsController < ApplicationController
     
     # Use your developer token and secret to instantiate access token object
     access_token = OAuth::AccessToken.new(consumer, user_token, user_secret)
-    @search=$content
+    @search=$content.content
     # Make a request for JSON data
     @json_txt = access_token.get("/v1/people-search?keywords=#{}", 'x-li-format' => 'json').body
     @profile = JSON.parse(@json_txt)
